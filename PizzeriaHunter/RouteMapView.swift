f7d281541e3721d96d7ff8edabf15e430fc92a7f
JanL <janelle.h.writes@gmail.com>
2016-06-15 19:38:29 -0500

    Create storyboard and begin connecting IBOutlets and IBActions

---
+++ PizzeriaHunter/RouteMapView.swift
@@ -0,0 +1,22 @@
+//
+//  RouteMapView.swift
+//  PizzeriaHunter
+//
+//  Created by joy on 6/15/16.
+//  Copyright © 2016 JanL. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class RouteMapView: MKMapView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
--- PizzeriaHunter/ViewController.swift
+++ PizzeriaHunter/ViewController.swift
@@ -7,19 +7,34 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController, MKMapViewDelegate, UITableViewDelegate, UITableViewDataSource, CLLocationManagerDelegate, UITextViewDelegate {
+
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
     }
-
+    
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    
+        let cell = tableView.dequeueReusableCellWithIdentifier("CellID", forIndexPath: indexPath)
+        
+        
+    return cell
+    
+    }
 
 }
 
